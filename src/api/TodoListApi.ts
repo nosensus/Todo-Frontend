a25233dcc1a6d21bba427b17b0f5ca639a9420d4
Alexander Kim <nosensus@gmail.com>
2024-03-28 21:24:17 +0500

    feat: save new item to db

--- src/api/TodoListApi.ts
+++ src/api/TodoListApi.ts
@@ -1,7 +1,18 @@
 import { API_URL } from "@todo/consts";
+import { TodoItem } from "@todo/store";
 
 const getTodoList = async () => {
   return await fetch(`${API_URL}/items`);
 }
 
-export { getTodoList }
+const postTodoListItem = async (newItem: TodoItem) => {
+  await fetch(`${API_URL}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newItem)
+  });
+}
+
+export { getTodoList, postTodoListItem }
